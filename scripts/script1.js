7763bf85327a0342b1dcd5bad2171209b353a33a
UsGomin <antoine.schoubert2@gmail.com>
2026-02-08 17:39:53 +0100

    changement

---
+++ scripts/script1.js
@@ -0,0 +1,14 @@
+
+
+// fadeout chargement
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const loader = document.getElementById("loader");
+    loader.classList.add("hide");
+
+    // retire comlpletement le loader
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 1000);
+});
