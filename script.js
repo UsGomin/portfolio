2ed4b871b53d931af32db78b3194eef68b9bea97
UsGomin <antoine.schoubert2@gmail.com>
2026-03-28 16:35:58 +0100

    ajout hero click me

---
+++ script.js
@@ -0,0 +1,11 @@
+(() => {
+    const $ = (id) => document.getElementById(id);
+    const hero = $('hero');
+    const heroButton = $('btn');
+
+    heroButton.addEventListener("click", () =>{ hero.setAttribute("hidden", ""); });
+    heroButton.addEventListener("click", () => {document.body.style.overflow = "auto"; });
+})();
+
+
+
